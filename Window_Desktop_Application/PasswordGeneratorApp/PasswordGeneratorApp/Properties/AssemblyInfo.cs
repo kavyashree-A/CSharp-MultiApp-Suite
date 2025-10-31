8bef1144f164b50490cc330edfd567629651ecda
HemanthPG501 <Hemanthpg501@gmail.com>
2025-10-31 21:59:18 +0530

    Add PasswordGeneratorApp project under Window_Desktop_Application

---
+++ Window_Desktop_Application/PasswordGeneratorApp/PasswordGeneratorApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PasswordGeneratorApp")]
+[assembly: AssemblyDescription("Simple Password Generator - .NET Framework 4.8")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Generated")]
+[assembly: AssemblyProduct("PasswordGeneratorApp")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d3b9f8a8-7b6f-4b1b-9c8a-1a2b3c4d5e6f")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
